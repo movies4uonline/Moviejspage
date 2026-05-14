9431c5ccf735bb02b1e7ef8e2818caa92adf14bb
movies4uonline <moviesu537@gmail.com>
2026-05-14 21:40:29 +0530

    Add movie data structure for 'Mr. X'

---
+++ movies.js
@@ -0,0 +1,32 @@
+const allMovies = [
+    { 
+      id: "mrx2026", 
+      title: "Mr. X", 
+      year: "2026",
+      lang: "MALAYALAM",
+      rating: "5.8",
+      mainImg: "https://image.url/main.jpg",
+      backImg: "https://image.url/back.jpg",
+          malayalam: [
+              { 
+                name: "1080p Full HD", 
+                w: "link_1080p", 
+                mx: "mx_link_1080p", 
+                dl: "dl_link_1080p",
+                tg: "link" 
+              },
+              { 
+                name: "1080p Full HD", 
+                w: "link_1080p", 
+                mx: "mx_link_1080p", 
+                dl: "dl_link_1080p",
+                tg: "link"
+              },
+              { 
+                name: "1080p Full HD", 
+                w: "link_1080p", 
+                mx: "mx_link_1080p", 
+                dl: "dl_link_1080p",
+                tg: "link" 
+              }
+          ],
